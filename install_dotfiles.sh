66ff1f30a2ba15f319249fabbb404eedbc4d8e94
Alexander V. Shulikov <shulik@metro.dec.net.ua>
2016-11-02 14:23:02 +0300

    Some changes

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc tmux.conf tmux-powerlinerc"    # list of files/folders to symlink in homedir
+files="vimrc tmux.conf tmux-powerlinerc"    # list of files/folders to symlink in homedir
 
 # http://habrahabr.ru/post/224979/
 mkdir ~/.vim
